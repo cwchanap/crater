d505b75de22809a7b49a8d122336a6170fb8e3c7
cwchanap <cwchanap@connect.ust.hk>
2025-09-08 19:58:03 -0700

    refactor: simplify Input and Textarea component props by removing redundant type definitions

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -6,19 +6,17 @@ export { Button, buttonVariants } from './components/button'
 export type { ButtonProps } from './components/button'
 
 export {
-  Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardDescription,
-  CardContent,
+    Card,
+    CardHeader,
+    CardFooter,
+    CardTitle,
+    CardDescription,
+    CardContent,
 } from './components/card'
 
 export { Input } from './components/input'
-export type { InputProps } from './components/input'
 
 export { Textarea } from './components/textarea'
-export type { TextareaProps } from './components/textarea'
 
 export { Badge, badgeVariants } from './components/badge'
 export type { BadgeProps } from './components/badge'
